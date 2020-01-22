540f1537139ec3b7975c941652c475f3fc50d96a
nicholasmontgomery <na_montgomery@yahoo.co.uk>
2020-01-22 11:51:35 +0000

    add pub_name_test

---
+++ pub.rb
@@ -0,0 +1,11 @@
+class Pub
+
+  attr_reader :name
+
+  def initialize(name, till, beers)
+    @name = name
+    @till = till
+    @beers = beers
+  end
+
+end
--- specs/pub_specs.rb
+++ specs/pub_specs.rb
@@ -3,6 +3,16 @@ require('minitest/reporters')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative('../pub')
+require_relative('../drink')
 
 class PubTest < MiniTest::Test
+  def setup
+    @drink1 = Drink.new("Budweiser", 3)
+    @drink2 = Drink.new("Magners", 4)
+    @pub1 = Pub.new("The Blue Blazer", 0, [@drink1, @drink2])
+  end
+
+  def test_get_pub_name
+    assert_equal("The Blue Blazer", @pub1.name)
+  end
 end
